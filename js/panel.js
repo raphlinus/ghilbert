e5294ffb7725f0b17bff7cd1d97d849242ba8f64
Adam Bliss <abliss@gmail.com>
2010-07-26 01:27:11 +0000

    Cleanup of table logic in panel, adding sorting column-headers.
    git-svn-id: https://ghilbert.googlecode.com/svn/branches/abliss@38 c014309e-2312-11df-8901-e16673284e38

--- js/panel.js
+++ js/panel.js
@@ -1,123 +1,130 @@
+GH.Table = function(tableElement) {
+    // ================ Private Variables ================
+    var table = tableElement;
+    var collection = [];
+    // map from row ID (== the first cell of the row) to TR element.
+    var rowMap = {};
+
+
+    function sortByIndex(index) {
+        collection.forEach(function(rowArr) {table.removeChild(rowMap[rowArr[0]]);});
+        collection.sort(function(a, b) {
+                            if (a[index] < b[index]) {
+                                return -1;
+                            } else if (a[index] > b[index]) {
+                                return 1;
+                            } else {
+                                return 0;
+                            }
+                        });
+        collection.forEach(function(rowArr) {table.appendChild(rowMap[rowArr[0]]);});
+    }
+    var header = document.createElement('tr');
+    function addHeader(name, sortIndex) {
+        var th = document.createElement('th');
+        var a = document.createElement('a');
+        a.innerHTML = name;
+        a.href='#';
+        a.onclick = function() { sortByIndex(sortIndex); return false;};
+        th.appendChild(a);
+        header.appendChild(th);
+    };
+    addHeader('Name', 0);
+    addHeader('Hyps', 1);
+    addHeader('Result', 2);
+    table.appendChild(header);
+
+    // ================ Public Functions ================
+    /**
+     * @param collection an array of table rows. Each row is an array
+     * containing the string values for the rows, plus one last value
+     * to be the onclick function for the row.  The first datum in each
+     * row is treated as rowId so it should be unique.
+     */
+    this.setContents = function(newCollection) {
+        var rowArr;
+        while ((rowArr = collection.pop())) {
+            table.removeChild(rowMap[rowArr[0]]);
+            delete rowMap[rowArr[0]];
+        }
+        newCollection.forEach(function(row) {
+                                  collection.push(row);
+                                  var tr = document.createElement('tr');
+                                  rowMap[row[0]] = tr;
+                                  row.forEach(function(cell) {
+                                                  if (typeof(cell) === 'function') {
+                                                      tr.onclick = cell;
+                                                  } else {
+                                                      var datum = document.createElement('td');
+                                                      datum.innerHTML = cell;
+                                                      tr.appendChild(datum);
+                                                  }
+                                              });
+                                  table.appendChild(tr);
+                              });
+    };
+};
+
 GH.Panel = function(ctx) {
     var self = this;
-    self.table = document.getElementById('panel');
     self.inferences = [];
     self.ctx = ctx;
+    self.table = new GH.Table(document.getElementById('panel'));
 
     function addStep(symName, sym) {
-	return function() {
-	    function appendText(text) {
-		window.direct.text.appendText(text);
-		window.direct.update();
-	    }
-	    appendText("\n");
-	    // Push on the mandatory hyps using their default names
-	    appendText("\n" + sym[4].map(function(mand) { return mand[2]; }).join(" "));
-	    // Push on the sym name itself
-	    appendText(" " + symName + "\n");
-	};
+        return function() {
+            function appendText(text) {
+                window.direct.text.appendText(text);
+                window.direct.update();
+            }
+            appendText("\n");
+            // Push on the mandatory hyps using their default names
+            appendText("\n" + sym[4].map(function(mand) { return mand[2]; }).join(" "));
+            // Push on the sym name itself
+            appendText(" " + symName + "\n");
+        };
     }
-    var row = document.createElement('tr');
-    function addChild(tag, contents) {
-	var datum = document.createElement(tag);
-	datum.innerHTML = contents;
-	row.appendChild(datum);
-	return datum;
-    }
-    addChild('th', 'Name');
-    addChild('th', 'Hyps').className = "hyp";
-    addChild('th', 'Result');
-    self.table.appendChild(row);
+    var inferenceCollection = [];
+    var deductionCollection = [];
+    self.inferences = inferenceCollection;
     for (var symName in ctx.syms) {
-	row = document.createElement('tr');
-	row.id = 'panel.row.' + symName;
-	var sym = ctx.syms[symName];
-	switch (sym[0]) {
-	case 'tvar':
-	case 'var':
-	    continue;
-	}
-	row.className += "sym " + sym[0];
-	if (sym[2].length > 0) {
-	    row.className += ' inference';
-	    self.inferences.push(symName);
-	} else {
-	    row.className += ' deduction';
-	}
-	addChild('td',symName);
-	//addChild('td',sym[1].map(GH.sexptounicode).join("<br/>"));
-	addChild('td',sym[2].map(GH.sexptounicode).join("<br/>")).className = "hyp";
-	addChild('td',GH.sexptounicode(sym[3]));
-	row.onclick = addStep(symName, sym);
-	self.table.appendChild(row);
+        var sym = ctx.syms[symName];
+        switch (sym[0]) {
+        case 'tvar':
+        case 'var':
+            continue;
+        }
+        var collection = (sym[2].length > 0) ? inferenceCollection : deductionCollection;
+        collection.push([symName,
+                         sym[2].map(GH.sexptounicode).join("<br/>"),
+                         GH.sexptounicode(sym[3]),
+                         addStep(symName, sym)]);
     }
-    function addStyleSheetRule(sIndex, selector, rule) {
-	var oStyleSheet = document.styleSheets[sIndex];
-
-	if (oStyleSheet.addRule) { // Internet Explorer
-	    oStyleSheet.addRule(selector, rule, oStyleSheet.rules.length);
-	} else { // w3c complient
-	    oStyleSheet.insertRule(selector + '{' + rule + '}',oStyleSheet.cssRules.length);
-	}
-    }
-    function showRows(className, show) {
-	addStyleSheetRule(0,'tr.' + className, 'display:' + (show ? 'table-row' : 'none'));
-    }
-    function showHyps(show) {
-	addStyleSheetRule(0,'.hyp', 'display:' + (show ? 'table-cell' : 'none'));
-    }
-    function clearUnified() {
-	if (self.unified) {
-	    self.unified.forEach(function(row) { self.table.removeChild(row); });
-	    self.unified = false;
-	}
-    }
-    addStyleSheetRule(0,'tr.sym', 'display:none');
     document.getElementById('inferences').onclick = function() {
-	clearUnified();
-	showRows('deduction', false);
-	showRows('inference', true);
-	showHyps(true);
+        self.table.setContents(inferenceCollection);
     };
     document.getElementById('deductions').onclick = function() {
-	clearUnified();
-	showRows('deduction', true);
-	showRows('inference', false);
-	showHyps(false);
+        self.table.setContents(deductionCollection);
     };
     document.getElementById('unified').onclick = function() {
-	clearUnified();
-	showRows('deduction', false);
-	showRows('inference', false);
-	showHyps(false);
-	self.unified = [];
-	self.inferences.forEach(
-	    function(symName) {
-		var sym = self.ctx.syms[symName];
-		var proofctx = window.direct.thmctx.proofctx;
-		var result;
-		try {
+        var collection = [];
+        self.inferences.forEach(
+            function(row) {
+                var symName = row[0];
+                var sym = self.ctx.syms[symName];
+                var proofctx = window.direct.thmctx.proofctx;
+                var result;
+                try {
                     var mandstack =  sym[4].map(function(mand) {
-                        return [mand, mand[2]]; // PICKUP
-                    });
-		    result = self.ctx.match_inference(sym, proofctx, mandstack);
-		} catch (e) {
-                    if (symName === 'gen') {
-                        alert("Rejecting gen based on " + e);
-                    }
-		    return;
-		}
-
-		var row = document.createElement('tr');
-		var datum = document.createElement('td');
-		datum.innerHTML = symName;
-		row.appendChild(datum);
-		datum = document.createElement('td');
-		datum.innerHTML = GH.sexptounicode(result);
-		row.appendChild(datum);
-		row.onclick = addStep(symName, sym);
-		self.table.appendChild(row);
-		self.unified.push(row);
-	    });
+                                                    return [mand, mand[2]];
+                                                });
+                    result = self.ctx.match_inference(sym, proofctx, mandstack);
+                    collection.push([symName, sym[2].length, GH.sexptounicode(result), addStep(symName, sym)]);
+                } catch (e) {
+                    // cannot unify.
+                    return;
+                }
+            });
+        self.table.setContents(collection);
     };
 };
