938ed201008d44f9c9d37db683248b768e048d85
Adam Bliss <abliss@gmail.com>
2011-03-20 19:48:34 +0000

    basic selection,redone
    git-svn-id: https://ghilbert.googlecode.com/svn/branches/abliss@118 c014309e-2312-11df-8901-e16673284e38

--- js/orcat_ui.js
+++ js/orcat_ui.js
@@ -2,13 +2,27 @@ exports.Ui = function(doc, theory) {
     var theoremsDiv = doc.getElementById("theorems");
     var treeDiv = doc.getElementById("tree");
     var theoremSpans = {};
+    // Makes a node selectable.  Note that it must have no margin, or else it will cause DOM movement.
+    function makeSelectable(node){
+            node.addEventListener('mouseover', function(e) {
+                                      node.className += " selected";
+                                      e.stopPropagation();
+                                  }, false);
+            node.addEventListener('mouseout', function(e) {
+                                      node.className = node.className.replace(/ selected/g,'');
+                                      e.stopPropagation();
+                                  }, false);
+
+    }
     // Make a tree out of a term.
     function makeTree(term, depth) {
         if (!depth) depth = 0;
+        var span;
         if (term.operator) {
             var op = term.operator();
             var tupleSpan = doc.createElement("span");
-            tupleSpan.className = "tuple";
+            tupleSpan.className += " tuple";
+            makeSelectable(tupleSpan);
             var opSpan = doc.createElement("span");
             opSpan.className = "operator";
             tupleSpan.appendChild(opSpan);
@@ -19,19 +33,23 @@ exports.Ui = function(doc, theory) {
             var n = op.numInputs();
             for (var i = 0; i < n; i++) {
                 var argSpan = makeTree(term.input(i), depth + 1);
-                argSpan.className = " arg";
+                argSpan.className += " arg";
                 argSpan.className += " argnum" + i;
                 argSpan.className += " argof" + n;
                 argSpan.className += " depth" + depth;
                 argsSpan.appendChild(argSpan);
             }
-            return tupleSpan;
+            span = tupleSpan;
         } else {
             var vSpan = doc.createElement("span");
-            vSpan.className = "variable";
+            vSpan.className = " variable";
+            makeSelectable(vSpan);
             vSpan.innerHTML = term.toString().replace(/^.*\./,'');
-            return vSpan;
+            span = vSpan;
         }
+        var outerSpan = doc.createElement("span"); 
+        outerSpan.appendChild(span);
+        return outerSpan;
     }
     // Make a tree and decorate it.
     function wrapTree(term) {
@@ -39,7 +57,7 @@ exports.Ui = function(doc, theory) {
         wrapperSpan.className = "wrapper";
         var span = makeTree(term);
         wrapperSpan.appendChild(span);
-        span.className = "theorem";
+        span.className += " theorem";
         return wrapperSpan;
     }
     this.reset = function() {
