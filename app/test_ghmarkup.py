edef5ae25b7228cf8b769a171305e9f9c32eeabb
Jim Kingdon <kingdon@panix.com>
2012-12-08 14:38:50 -0800

    Add some tests for the wiki markup.

---
+++ app/test_ghmarkup.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+
+import unittest
+import ghmarkup
+
+class test_ghmarkup(unittest.TestCase):
+  def test_unadorned_text(self):
+    self.assertEqual("<p>\nfoo\n</p>", ghmarkup.ghmarkup("foo"))
+
+  def test_bold(self):
+    self.assertEqual("<p>\nthe <strong>quick</strong> brown fox\n</p>", ghmarkup.ghmarkup(
+      "the *quick* brown fox"))
+
+  def test_bullet(self):
+    self.assertEqual("""<ul>
+<li>
+<div>
+one
+</div>
+</li>
+</ul>""", ghmarkup.ghmarkup("* one"))
+
+  def test_two_bullets(self):
+    self.assertEqual("""<ul>
+<li>
+<div>
+one
+</div>
+</li>
+<li>
+<div>
+two
+</div>
+</li>
+</ul>""", ghmarkup.ghmarkup("* one\n* two\n"))
+
+  def test_math(self):
+    self.assertEqual(u'<p>\n<span class="sexp">(→ p q)</span>\n</p>', ghmarkup.ghmarkup("#(→ p q)#"))
+
+  def test_literal(self):
+    self.assertEqual(u'<p>\n<tt>#(→ p q)#</tt>\n</p>', ghmarkup.ghmarkup("{{{#(→ p q)#}}}"))
+
